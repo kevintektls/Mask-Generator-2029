d9833e95a01d5587a068f4acf11aa57e983e7d05
Kevin SALANIER <kevin.salanier@epitech.eu>
2026-06-11 08:34:01 +0200

    feat (controller.py): define gamepad control axes for Logitech F710

--- scripts/controller.py
+++ scripts/controller.py
@@ -9,3 +9,7 @@ sys.path.insert(0, '/home/robotcar/Gamepad')
 import Gamepad # gamepad is for logitech F710 connected in usb mode with dungle
 from pyvesc import VESC # pyvesc is for cruise control connected in usb mode
 
+GAMEPAD_TYPE = Gamepad.Xbox360 # Logitech F710 has two modes, and the mode X simulate Xbox360 controller
+AXIS_FORWARD = "RIGHT-TRIGGER"
+AXIS_BACKWARD = "LEFT-TRIGGER"
+AXIS_STEERING  = 'RIGHT-X'
