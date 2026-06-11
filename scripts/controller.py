6d4f0fc19be42e976629ef43c5e6145f2baf145f
Kevin SALANIER <kevin.salanier@epitech.eu>
2026-06-11 08:31:12 +0200

    feat (controller.py): organize imports and add Gamepad library path

--- scripts/controller.py
+++ scripts/controller.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+import sys
+import time
+import gc
+
+# I installed the Gamepad library in a custom location, so I need to add it to the path
+sys.path.insert(0, '/home/robotcar/Gamepad')
+
+import Gamepad # gamepad is for logitech F710 connected in usb mode with dungle
+from pyvesc import VESC # pyvesc is for cruise control connected in usb mode
+
