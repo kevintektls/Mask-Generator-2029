470651232ec408212d5fc255a7346f202c799e18
Nolhan82 <nolhan.schneider@epitech.eu>
2026-06-30 22:26:54 +0200

    put crop to 0.35

--- scripts/vision_preprocess.py
+++ scripts/vision_preprocess.py
@@ -13,7 +13,7 @@ import cv2
 import numpy as np
 
 # IMPORTANT : garder les mêmes paramètres partout
-CROP_TOP_RATIO = 0.45
+CROP_TOP_RATIO = 0.35
 
 # Alignement manuel caméra droite -> caméra gauche
 STEREO_DX = -15.0
